e6405453eb3acfe98c6f4e9db1590592ad7b785f
Mid <83442808+Miduwu@users.noreply.github.com>
2022-03-04 16:53:44 -0600

    Create ukraine.js

---
+++ commands/ukraine.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'ukraine',
+    code: `
+$color[1;RANDOM]
+$description[1;**#Ukraine**]
+$imageAPI[1;overlay;{
+"image1": "$userAvatar[$mentioned[1;yes]]",
+"image2": "https://cdn.discordapp.com/attachments/932112450801786940/949435518607843369/UKRAINE.png",
+"opacity": 50
+}]
+`
+}
