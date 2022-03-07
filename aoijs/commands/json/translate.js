b658e6b8f5c62859f68da8d5a8b70f4f7c6197c8
Mid <83442808+Miduwu@users.noreply.github.com>
2022-03-06 20:15:23 -0600

    Update translate.js

--- aoijs/commands/json/translate.js
+++ aoijs/commands/json/translate.js
@@ -3,6 +3,8 @@ module.exports = {
     aliases: ["tr", "translator"],
     code: `
     $color[1;RANDOM]
-    $description[1;$requestAPI[json;translate;{"text": "$messageSlice[1]", "source": "auto", "target": "$message[1]"};data.translated;Sorry but i can't translate that]]
+    $title[1;From $getProperty[data.source] to $getProperty[data.target]]
+    $description[1;$getProperty[data.translated]]
+    $requestAPI[json;translate;{"text": "$messageSlice[1]", "source": "auto", "target": "$message[1]"};Sorry but i can't translate that]
     `
 }
